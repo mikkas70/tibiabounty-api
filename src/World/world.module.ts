9eaf8a4b476965c5d13461557579a4d39cedfd43
Micael Sousa <micael.sousa@namecheap.com>
2024-10-22 23:00:44 +0100

    Fixes awesoemine

--- src/World/world.module.ts
+++ src/World/world.module.ts
@@ -3,13 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WorldController } from './world.controller';
 import { World, WorldSchema } from './world.schema';
 import { WorldService } from './world.service';
-import { TibiaModule } from '../Tibia/tibia.module';
 import { WorldEvent } from './world.event';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: World.name, schema: WorldSchema }]),
-    TibiaModule,
   ],
   controllers: [WorldController],
   providers: [WorldService, WorldEvent],
